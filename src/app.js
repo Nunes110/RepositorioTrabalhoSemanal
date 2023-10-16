9e1ab54cc76e21007643078103f5f9e95a9327b8
Nunes110 <arturdesouzanunes@gmail.com>
2023-10-16 08:22:27 -0300

    passo 4

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-const router = require('./routes/rotas');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const router = require('.routes/rotas');
+
+app.set('port', process.env.PORT || 3333);
+
+module.exports = app;
+
 app.use('/api', router);
